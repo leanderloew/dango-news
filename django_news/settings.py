df7bf8a044e371f1fafb239ad58d3e6f1b93229e
lysecret2 <leanderlow@yahoo.de>
2020-05-10 16:37:40 +0200

    managed to get a working version of the initial landing page. Can be modified to act as a base

--- django_news/settings.py
+++ django_news/settings.py
@@ -120,5 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ( os.path.join('static'), )
+
 AUTH_USER_MODEL="users.CustomUser"
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
