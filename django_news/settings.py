c43ec68d6ef0b1c77fbc35e86b89b3011715987e
lysecret2 <leanderlow@yahoo.de>
2020-05-11 14:58:48 +0200

    added expose model

--- django_news/settings.py
+++ django_news/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+    'exposes.apps.ExposesConfig',
 
 ]
 
--- exposes/models.py
+++ exposes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.urls import reverse
+
+
+class Expose(models.Model):
+    title = models.CharField(max_length=255)
+    pdf = models.FileField(upload_to="pdf")
+    description = models.TextField()
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+    def __str__(self):
+        return str(self.title) if self.title else ''
+
+    def get_absolute_url(self):
+        return reverse("article_detail",args=[str(self.id)])
