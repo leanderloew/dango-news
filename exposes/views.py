fa53bf55bd3737d3a3aef51009ecdd29a4ae2474
lysecret2 <leanderlow@yahoo.de>
2020-05-12 12:14:57 +0200

    basic website running switch to docker in new project

--- exposes/views.py
+++ exposes/views.py
@@ -3,27 +3,33 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import ListView,DeleteView,DetailView,UpdateView,CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Expose
 
-class ExposeListView(ListView):
+class ExposeListView(LoginRequiredMixin,ListView):
     model = Expose
     template_name = "expose_list.html"
 
-class ExposeDetailView(DetailView):
+class ExposeDetailView(LoginRequiredMixin,DetailView):
     model = Expose
     template_name = "expose_detail.html"
 
-class ExposeUpdateView(UpdateView):
+class ExposeUpdateView(LoginRequiredMixin,UpdateView):
     model = Expose
     fields = ("title","pdf","description")
     template_name = "expose_edit.html"
 
-class ExposeDeleteView(DeleteView):
+class ExposeDeleteView(LoginRequiredMixin,DeleteView):
     model = Expose
     template_name = "expose_delete.html"
     success_url = reverse_lazy("expose_list")
 
-class ExposeCreateView(CreateView):
+class ExposeCreateView(LoginRequiredMixin,CreateView):
     model = Expose
     template_name = "expose_new.html"
-    fields = ("title","pdf","description","author")
+    fields = ("title","pdf","description")
+
+    def form_valid(self, form):
+        #request reflects the incomming data
+        form.instance.author=self.request.user
+        return super().form_valid(form)
